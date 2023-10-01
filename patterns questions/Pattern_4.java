7766d70aecb5f4a6ba3b92c27abe662919440274
Chhavimohitkar65 <chhavi.mohitkar06@gmail.com>
2023-10-01 12:28:53 +0530

    update

--- patterns questions/Pattern_4.java
+++ patterns questions/Pattern_4.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class pattern_3 {
+public class Pattern_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n");
         int n = sc.nextInt();
-
         int i = 1;
+        int counting = 1;
         while (i <= n) {
             int j = 1;
             while (j <= n) {
-                System.out.print(j);
+                System.out.print(counting);
+                counting++;
                 j++;
             }
             System.out.println();
