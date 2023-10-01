d2b1d44340374d60d4a022d6c2e4da92595d5e1b
Chhavimohitkar65 <chhavi.mohitkar06@gmail.com>
2023-10-02 01:12:51 +0530

    update

---
+++ patterns questions/pattern_16.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class pattern_16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n ");
+        int n = sc.nextInt();
+
+        int row = 1;
+        while(row<=n){
+
+            //printing the space
+            int space = 1;
+            while(space<=n-row){
+                System.out.print(" ");
+                space++;
+            }
+
+            //printing the stars
+            int col = 1;
+            while(col<=row) {
+                System.out.print("*");
+                col++;
+            }
+            System.out.println();
+            row++;
+        }
+
+    }
+}
