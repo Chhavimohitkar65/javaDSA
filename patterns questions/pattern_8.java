4a74c00403dc20bc3592b12f50ae9c36db903bef
Chhavimohitkar65 <chhavi.mohitkar06@gmail.com>
2023-10-01 14:41:21 +0530

    new

--- patterns questions/pattern_8.java
+++ patterns questions/pattern_8.java
@@ -6,5 +6,16 @@ public class pattern_8 {
         System.out.println("enter the value of n");
         int n = sc.nextInt();
 
+        int i = 1;
+        while(i<=n){
+           int j = 1;
+           while (j<=i){
+            System.out.print(i-j+1);
+            j++;
+           }
+           System.out.println();
+           i++;
+        }
+
     }
 }
