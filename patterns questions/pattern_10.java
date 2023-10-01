19c732dc5489bd10b3db5be3cc1a31aaa157840f
Chhavimohitkar65 <chhavi.mohitkar06@gmail.com>
2023-10-01 15:29:25 +0530

    update

---
+++ patterns questions/pattern_10.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class pattern_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n");
+        int n = sc.nextInt();
+
+        int row = 1;
+        while(row<=n){
+            int col = 1;
+            while(col<=n){
+                char ch = (char)('A'+ col-1);
+                System.out.print(ch);
+                col++;
+            }
+            System.out.println();
+            row++;
+        }
+
+    }
+}
---
+++ patterns questions/pattern_9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class pattern_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n ");
+        int n = sc.nextInt();
+         
+        int row = 1;
+        while(row<=n){
+            int col = 1;
+            char ch = (char)('A'+ row - 1); //calculate the character based on the row number
+            while(col<=n){
+                System.out.print(ch);
+                col++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+}
